da9b0a22a82e3aaba4e9d7075b1cef67cbc6a9d4
Cheng Wei Lun <138444377+luuuneytunes12@users.noreply.github.com>
2025-10-17 22:47:22 +0800

    completed automated eda

--- src/plots.py
+++ src/plots.py
@@ -1,29 +1,31 @@
-from pathlib import Path
+# ==========================================
+# Setting rcParams for Matplotlib
+# ==========================================
+from cycler import cycler
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
-from loguru import logger
-from tqdm import tqdm
-import typer
+# Define custom colour cycle using tab10 palette
+colors = cycler(color=plt.get_cmap("tab10").colors)  # ['#1f77b4', '#ff7f0e', '#2ca02c', ...]
 
-from src.config import FIGURES_DIR, PROCESSED_DATA_DIR
+# Set overall style and appearance
+mpl.style.use("ggplot")  # Use ggplot-style background and grid
 
-app = typer.Typer()
+# Global figure settings
+mpl.rcParams["figure.figsize"] = (20, 5)  # Default figure size (width=20, height=5)
+mpl.rcParams["figure.dpi"] = 100  # Resolution (dots per inch)
+mpl.rcParams["figure.titlesize"] = 25  # Default title font size
 
+# Axes and grid styling
+mpl.rcParams["axes.facecolor"] = "white"  # Plot background colour
+mpl.rcParams["axes.grid"] = True  # Enable grid
+mpl.rcParams["grid.color"] = "lightgray"  # Grid line colour
+mpl.rcParams["axes.prop_cycle"] = colors  # Set colour cycle for plots
+mpl.rcParams["axes.linewidth"] = 1  # Border thickness
 
-@app.command()
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
-    output_path: Path = FIGURES_DIR / "plot.png",
-    # -----------------------------------------
-):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Generating plot from data...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Plot generation complete.")
-    # -----------------------------------------
+# Axis tick and label colours
+mpl.rcParams["xtick.color"] = "black"
+mpl.rcParams["ytick.color"] = "black"
 
-
-if __name__ == "__main__":
-    app()
+# Font settings
+mpl.rcParams["font.size"] = 12  # Base font size for labels, ticks, etc.
